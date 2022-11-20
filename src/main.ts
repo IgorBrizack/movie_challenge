ac28f32b209a73cd21bad4ff307544067a94c329
Igor Brizack <igor_brizack@hotmail.com>
2022-11-20 20:56:52 -0300

    trying to fix deploy

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Server is Running');
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
