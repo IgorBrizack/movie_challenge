6f0892c6f4222f18f005fe95c5a0219bb843d5fd
Igor Brizack <igor_brizack@hotmail.com>
2022-11-20 18:36:21 -0300

    feat: api done

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,19 +2,47 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
+//REF https://www.youtube.com/watch?v=dXOfOgFFKuY&ab_channel=MariusEspejo
 describe('UsersController', () => {
   let controller: UsersController;
+
+  const mockUsersService = {
+    create: jest.fn((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
-    }).compile();
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Deve criar um usuário', () => {
+    expect(
+      controller.create({
+        name: 'Igor',
+        email: 'igormail@mail.com',
+        password: '12345',
+      }),
+    ).toEqual({
+      id: expect.any(Number),
+      name: 'Igor',
+      email: 'igormail@mail.com',
+      password: '12345',
+    });
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,6 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
